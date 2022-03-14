03a785c4c9a327af4cb3ab644ec23ce2b440bbb9
Felix Chen <fechen1993@gmail.com>
2022-03-14 10:40:33 -0400

    Add pop up form functionality

--- app.js
+++ app.js
@@ -1,40 +1,60 @@
 let myLibrary = [];
 const cardSection = document.querySelector('.card-section');
 
-function Book(title,author,pages,isRead) {
-this.title = title;
-this.author = author;
-this.pages = pages;
-this.isRead = isRead
+function Book(title, author, pages) { //Book object constructor 
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
 }
 
-const newBookButton = document.querySelector('.new-book-button');
+const newBookButton = document.querySelector('.new-book-button'); //button to bring up the add book form
+newBookButton.addEventListener('click', function(e){
+    document.querySelector('.book-input-form').style.display = "block";
+});
 
-newBookButton.addEventListener('click', function (e) {
-   const newTitle = prompt('Enter a title');
-   const newAuthor = prompt('Enter an author');
-   let newPages = +prompt('Enter number of pages');
-   let newIsRead = prompt('Enter yes or no');
-   if(newIsRead.toLowerCase() === 'yes') {
-       newIsRead = true;
-   }
-   const newBook = new Book(newTitle,newAuthor,newPages,newIsRead);
-   addBookToLibrary(newBook);
-   createBookCard(newBook);
-   console.log(typeof Object.keys(newBook)[0]); //This accesses the key names of the object so that I can use it to print the card attributes
+const closeFormButton = document.querySelector('.close-form-button');
+closeFormButton.addEventListener('click', function (e) {
+    document.querySelector('.book-input-form').style.display = "none";
 })
 
-function addBookToLibrary (newBook) {
+const addBookButton = document.querySelector('.add-book-button'); //button to submit book info
+
+
+addBookButton.addEventListener('click', function (e) {
+    // e.preventDefault();
+    const titleInputBox = document.querySelector('#title');
+    const authorInputBox = document.querySelector('#author')
+    const pagesInputBox = document.querySelector('#pages');
+ 
+
+    const newTitle = titleInputBox.value;
+    const newAuthor = authorInputBox.value;
+    const newPages = pagesInputBox.value;
+  
+
+    const newBook = new Book(newTitle, newAuthor, newPages);
+
+    addBookToLibrary(newBook);
+    createBookCard(newBook);
+
+    titleInputBox.value = "";
+    authorInputBox.value = "";
+    pagesInputBox.value = "";
+
+});
+
+function addBookToLibrary(newBook) {
     myLibrary.push(newBook);
 }
 
 function createBookCard(newBook) {
     const bookCard = document.createElement('div');
-    for (let i=0; i<=Object.keys(newBook).length-1;i++) {
+    for (let prop in newBook) {
         const cardAttribute = document.createElement('p');
-        cardAttribute.innerText = Object.keys(newBook)[i];
+        cardAttribute.innerText = newBook[prop];
         bookCard.appendChild(cardAttribute);
     }
+
     bookCard.classList.add('book-card-container');
     cardSection.appendChild(bookCard);
 }
